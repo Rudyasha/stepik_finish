0c1615621482c21c1f7cec66f0785b7fe946f8d1
Rudyasha <55089144+Rudyasha@users.noreply.github.com>
2020-05-23 16:25:52 +0300

    second commit

---
+++ test_items.py
@@ -0,0 +1,8 @@
+﻿import pytest
+import time
+
+def test_guest_should_see_basket_btn(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    #time.sleep(30)
+    assert browser.find_element_by_id("add_to_basket_form"), "Should be a butten"
